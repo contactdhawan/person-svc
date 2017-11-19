805f69daf808210d969a357e7c08f123a8eb8800
Neeraj <contactdhawan@gmail.com>
2017-11-18 21:58:36 -0600

    exception class

---
+++ demo/src/main/java/sf/crom/exception/HTTP404Exception.java
@@ -0,0 +1,26 @@
+package sf.crom.exception;
+
+// 404 not found
+public class HTTP404Exception extends RuntimeException {
+
+	public HTTP404Exception() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public HTTP404Exception(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public HTTP404Exception(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public HTTP404Exception(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
